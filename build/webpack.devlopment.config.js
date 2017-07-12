ab97248c450e0d3088bdc566984f1047f77d0233
stoneyong <stone.zou@mingdao.com>
2017-07-13 00:36:26 +0800

    devServer

--- build/webpack.devlopment.config.js
+++ build/webpack.devlopment.config.js
@@ -5,6 +5,10 @@ const baseConfig = require('./webpack.base.config');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const devServer = require('webpack-dev-server');
 
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
 const config = Object.assign({}, baseConfig, {
   cache: true,
   watch: true,
@@ -13,6 +17,10 @@ const config = Object.assign({}, baseConfig, {
     new ProgressBarPlugin({ format: ' webpack 构建中 [:bar] :percent :msg' }),
     new webpack.optimize.OccurrenceOrderPlugin(),
   ]),
-
+  devServer: {
+    contentBase: resolve('dist'),
+    publicPath: '/',
+    hot: true
+  }
 });
 module.exports = config;
