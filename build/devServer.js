12dc64a4202c13a41fce267a48ae534fa5443212
stoneyong <stone.zou@mingdao.com>
2017-07-14 01:19:43 +0800

    '添加devServer'

--- build/devServer.js
+++ build/devServer.js
@@ -1,7 +1,28 @@
-if(!process.env.NODE_EVN) {
+if (!process.env.NODE_EVN) {
     process.env.NODE_EVN = 'development';
 }
-const express = require('express');
 const webpack = require('webpack');
 const path = require('path');
+const devServer = require('webpack-dev-server');
+const configDev = require('./webpack.devlopment.config');
+
+const compiler = webpack(configDev);
+
+const server = new devServer(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    contentBase: path.client(),
+    hot: true,
+    quiet: false,
+    noInfo: false,
+    lazy: false,
+    stats: {
+        chunks : false,
+        chunkModules : false,
+        colors : true
+    }
+});
+
+server.listen(10001, "127.0.0.1", function() {
+	console.log("Starting server on http://localhost:10001");
+});
 
--- build/webpack.devlopment.config.js
+++ build/webpack.devlopment.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const path = require('path');
 const baseConfig = require('./webpack.base.config');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
-const devServer = require('webpack-dev-server');
+
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -17,13 +17,5 @@ const config = Object.assign({}, baseConfig, {
     new ProgressBarPlugin({ format: ' webpack 构建中 [:bar] :percent :msg' }),
     new webpack.optimize.OccurrenceOrderPlugin(),
   ]),
-  devServer: {
-    contentBase: resolve('dist'),
-    publicPath: '/',
-    hot: true,
-    inline: true,
-    port: 10001,
-    host:'localhost',
-  }
 });
 module.exports = config;
